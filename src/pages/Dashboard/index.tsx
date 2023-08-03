2f095bf2ec46826590896f64397da1fe511445c2
GSRJ <gerson.contato@outlook.com>
2023-08-02 21:18:39 -0300

    feat: Dashboard initial structure

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,44 @@
-export const Dashboard = () => {
-  return <h1>Dashboard</h1>
+import { useEffect, useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { api } from "../../services/api";
+
+export interface IContact {
+  id: number;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  createdAt: string;
+  user: {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    phone: string;
+    password: string;
+    admin: boolean;
+    createdAt: string;
+  };
 }
+
+export const Dashboard = () => {
+  const [contacts, setContacts] = useState<IContact[]>();
+  const { loading } = useAuth();
+  useEffect(() => {
+    (async () => {
+      const response = await api.get<IContact[]>("/contacts");
+      setContacts(response.data);
+    })();
+  }, []);
+  return (
+    <>
+      {" "}
+      <h1>Dashboard</h1>;
+      <ul>
+        {contacts?.map((contact) => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TLoginData } from "../pages/Login/validator";
 import { api } from "../services/api";
@@ -9,15 +9,18 @@ interface IAuthProviderProps {
 
 interface IAthContextValues {
   signIn: (data: TLoginData) => Promise<void>;
+  loading: boolean;
 }
 export const AuthContext = createContext({} as IAthContextValues);
 
 export const AuthProvider = ({ children }: IAuthProviderProps) => {
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
+      setLoading(false);
       navigate("/login");
       return;
     }
@@ -29,7 +32,7 @@ export const AuthProvider = ({ children }: IAuthProviderProps) => {
       const response = await api.post("/login", data);
       const { token } = response.data;
       api.defaults.headers.common.Authorization = `Bearer ${token}`;
-
+      setLoading(false);
       localStorage.setItem("token", token);
 
       navigate("dashboard");
